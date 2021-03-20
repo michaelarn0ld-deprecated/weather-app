7199daa968867e6d499774b91798e50d87075160
Michael Arnold <marnolddevelop@gmail.com>
2021-03-19 17:44:22 -0700

    added GSAP plugin to dynamically style search bar on form submission

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { gsap, Power3 } from 'gsap';
 import './App.scss';
 
 function App() {
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+
+  useEffect(() => {
+    console.log('loaded');
+  }, [query]);
+
+  useEffect(() => {
+    return () => {
+      let tl = gsap.timeline();
+      tl.to('.upper-content-header', { opacity: 0, y: -100, delay: 0.5 });
+      tl.to('.search-bar', { ease: Power3.easeInOut, y: -300, duration: 1 });
+    };
+  }, [hasSubmitted]);
+
+  const updateSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const getSearch = (event) => {
+    event.preventDefault();
+    setQuery(search);
+    setSearch('');
+    setHasSubmitted(true);
+  };
+
   return (
     <div className="App">
       <div className="upper-content">
-        <h1>Weather App</h1>
+        <h1 className="upper-content-header">Weather App</h1>
       </div>
-      <div className="search-bar">
+      <form className="search-bar" onSubmit={getSearch}>
         <input
           type="text"
           name=""
-          id=""
+          value={search}
+          onChange={updateSearch}
           className="search-bar-input"
           placeholder="Enter a city..."
         />
         <button className="search-bar-submit">
-          <i class="fas fa-search"></i>
+          <i className="fas fa-search"></i>
         </button>
-      </div>
+      </form>
     </div>
   );
 }
