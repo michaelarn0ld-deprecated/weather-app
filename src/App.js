1a736b0581059d35900652f731404d1150e70647
Michael Arnold <marnolddevelop@gmail.com>
2021-03-24 22:06:51 -0700

    asynch/await to fetch data from api; render components upon retrieval

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import CurrentWeather from './CurrentWeather';
 import { useEffect, useState } from 'react';
 import { gsap, Power3 } from 'gsap';
 import './App.scss';
 
 function App() {
+  const API_KEY = 'd7e8027877484293a6a40033211803';
   const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [weather, setWeather] = useState([]);
   const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('chicken');
+  const [query, setQuery] = useState('Los Angeles');
 
   useEffect(() => {
-    console.log('loaded');
+    getRecipe();
   }, [query]);
 
   useEffect(() => {
@@ -17,8 +20,21 @@ function App() {
       let tl = gsap.timeline();
       tl.to('.upper-content-header', { opacity: 0, y: -100, delay: 0.5 });
       tl.to('.search-bar', { ease: Power3.easeInOut, y: -300, duration: 1 });
+      tl.to('.current-weather', {
+        ease: Power3.easeInOut,
+        opacity: 1,
+        duration: 0.5,
+      });
     };
   }, [hasSubmitted]);
+
+  const getRecipe = async () => {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${query}&days=3&aqi=yes&alerts=no`
+    );
+    const data = await response.json();
+    setWeather(data);
+  };
 
   const updateSearch = (event) => {
     setSearch(event.target.value);
@@ -49,6 +65,7 @@ function App() {
           <i className="fas fa-search"></i>
         </button>
       </form>
+      {hasSubmitted && <CurrentWeather data={weather} />}
     </div>
   );
 }
